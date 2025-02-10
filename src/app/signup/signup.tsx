dd49b8aff93826c3f7063a78c1578f0b97ac7a4e
Sristy17 <136837204+sristy17@users.noreply.github.com>
2025-02-10 23:44:33 +0530

    Set up

---
+++ src/app/signup/signup.tsx
@@ -0,0 +1,9 @@
+export default function Signup() {
+    return(
+        <>
+        <div className="flex justify-left h-20 w-20 ">
+            <img src="/logo.png" alt="logo" />
+        </div>
+        </>
+    )
+}

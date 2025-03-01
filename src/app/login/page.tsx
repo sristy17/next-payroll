bd592ca79b58258b4e2334362736e5459a24592a
Sristy17 <136837204+sristy17@users.noreply.github.com>
2025-03-02 00:42:58 +0530

    Auth build!

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from 'next/image';
+import Link from 'next/link';
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { createClient } from "@supabase/supabase-js";
@@ -33,7 +34,7 @@ export default function Login() {
     }
 
      // Authenticate user using Supabase auth
-     const { data, error } = await supabase.auth.signInWithPassword({
+     const { error } = await supabase.auth.signInWithPassword({
       email,
       password,
     });
@@ -43,7 +44,6 @@ export default function Login() {
       return;
     }
     router.push("/");
-  
   };
 
     return (
@@ -54,14 +54,12 @@ export default function Login() {
           <div className="absolute top-28 left-32 w-28 h-28 rounded-3 mb-5">
               <Image src={logo} alt="logo" />
           </div>
-            </div>
-            <div className="absolute left-36 top-60">
-              <h1 className="text-3xl font-bold">Getting Started</h1>
-              <p className="text-gray-400">Welcome back to Next Pay - Login to your account</p>
-              <div>
-
-              <form onSubmit={handleLogin}>
-
+        </div>
+        <div className="absolute left-36 top-60">
+          <h1 className="text-3xl font-bold">Getting Started</h1>
+          <p className="text-gray-400">Welcome back to Next Pay - Login to your account</p>
+          <div>
+            <form onSubmit={handleLogin}>
               <div className="absolute top-24">
                   <label htmlFor="email" className="text-md font-medium text-gray-100">
                     Email
@@ -78,67 +76,60 @@ export default function Login() {
                   />
                 </div>
 
-
-                <div className="absolute top-44">
-                  <label htmlFor="password" className="text-md font-medium text-gray-100">
-                    Password
-                  </label>
-                  <a href="/forgotpassword" className="absolute top-0 left-60 ml-6 text-sm font-bold text-white">
-                    Forgot?
-                  </a>
-                  <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    required
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    className="mt-1 bg-transparent w-80 px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-                    placeholder="Enter your password"
-                  />
-                </div>
-
-
-                <a href="/">
-                <button
-                  type="submit"
-                  className="absolute top-64 w-full py-2 px-4 bg-gradient-to-br from-green-700 to-black text-white font-semibold rounded-md shadow-md focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-                >
-                  Log In
-                </button>
-                </a>
-
-              </form>
-
-              {error && <p className="text-red-500 absolute top-72">{error}</p>}
-               
-                <p className="text-gray-400 absolute top-72 m-5">
-                  Do not have an Account?
-                  <a href="/signup" className="absolute top-0 ml-3 font-bold text-white">
-                    Signup
-                  </a>
-                </p>
+              <div className="absolute top-44">
+                <label htmlFor="password" className="text-md font-medium text-gray-100">
+                  Password
+                </label>
+                <Link href="/forgotpassword" className="absolute top-0 left-60 ml-6 text-sm font-bold text-white">
+                  Forgot?
+                </Link>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="mt-1 bg-transparent w-80 px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+                  placeholder="Enter your password"
+                />
               </div>
-            </div>
+
+              <button
+                type="submit"
+                className="absolute top-64 w-full py-2 px-4 bg-gradient-to-br from-green-700 to-black text-white font-semibold rounded-md shadow-md focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+              >
+                Log In
+              </button>
+            </form>
+
+            {error && <p className="text-red-500 absolute top-72">{error}</p>}
+
+            <p className="text-gray-400 absolute top-72 m-5">
+              Do not have an Account?
+              <Link href="/signup" className="absolute top-0 ml-3 font-bold text-white">
+                Signup
+              </Link>
+            </p>
           </div>
-  
-          {/* Right Section */}
-          <div className="w-[45%] bg-gradient-to-br rounded-3xl m-2 from-green-900 to-black flex items-right justify-end absolute right-0 top-0 bottom-0">
-            <div className="absolute top-16 left-10">
+        </div>
+
+        {/* Right Section */}
+        <div className="w-[45%] bg-gradient-to-br rounded-3xl m-2 from-green-900 to-black flex items-right justify-end absolute right-0 top-0 bottom-0">
+          <div className="absolute top-16 left-10">
             <h1 className="font-extrabold text-white text-6xl">
               Enter 
-             <span className="block">the Future</span>
-             <span className="block"> of Payments,</span>
-             <span className="block">today</span>
+              <span className="block">the Future</span>
+              <span className="block"> of Payments,</span>
+              <span className="block">today</span>
             </h1>
-            </div>
-
-            <div className="absolute top-80 left-96 mt-10 w-48 h-48 rounded-3 mb-5">
-            <Image src={logo} alt="logo" />
-            </div>
-          
-
           </div>
-      </>
-    );
+
+          <div className="absolute top-80 left-96 mt-10 w-48 h-48 rounded-3 mb-5">
+            <Image src={logo} alt="logo" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }

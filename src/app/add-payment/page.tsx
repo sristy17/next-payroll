114cf59277fd07658ae4e4659cdfca568be10940
Pooranjoy <pooranjoyb2016@gmail.com>
2025-07-08 00:25:37 +0530

    fix: imports

--- src/app/add-payment/page.tsx
+++ src/app/add-payment/page.tsx
@@ -1,11 +1,7 @@
 import Sidebar from '@/components/sidebar';
-import Navbar from '@/components/navbar';
 
 export default function AddPayment() {
     return (
-        <>
         <Sidebar />
-        <Navbar />
-        </>
     );
 }
--- src/app/gst-return/page.tsx
+++ src/app/gst-return/page.tsx
@@ -1,11 +1,7 @@
 import Sidebar from '@/components/sidebar';
-import Navbar from '@/components/navbar';
 
 export default function GstReturn(){
     return(
-        <>
        <Sidebar />
-       <Navbar />
-        </>
     )
 }

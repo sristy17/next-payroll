ac839a6c47f7da73928df4335dc04f83b462afa0
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-08-01 23:10:29 +0530

    feat(ui): implement AddPayment component with payment form and success handling

--- src/app/add-payment/page.tsx
+++ src/app/add-payment/page.tsx
@@ -1,7 +1,31 @@
-import Sidebar from '@/components/sidebar';
+"use client";
+import Sidebar from "@/components/sidebar";
+import PaymentPage, { PaymentFormData } from "./_components/payment-page";
+import { useState } from "react";
+import PaymentSuccess from "./_components/payment-success";
 
 export default function AddPayment() {
-    return (
-        <Sidebar />
-    );
+  const [success, setSuccess] = useState(false);
+  const handleSubmit = async (data: PaymentFormData) => {
+    console.log(data);
+    setSuccess(true);
+  };
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <main className="container mx-auto flex-1 p-6 md:p-8">
+        <div className="flex flex-col gap-y-2 my-4">
+          <h1 className="font-bold text-xl md:text-3xl">Hello, Anna!</h1>
+          <p className="text-muted-foreground">
+            Send and recieve funds with pleasure.
+          </p>
+        </div>
+        {success ? (
+          <PaymentSuccess />
+        ) : (
+          <PaymentPage handleSubmit={handleSubmit} />
+        )}
+      </main>
+    </div>
+  );
 }

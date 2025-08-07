67b00d15d16c90729d6c8feffa6dc64af4303997
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-08-07 12:04:53 +0530

    refactor: Implemented Payment page and PaymentSuccess page in /add-payment route page and handled success using useState

--- src/app/(user)/add-payment/page.tsx
+++ src/app/(user)/add-payment/page.tsx
@@ -1,6 +1,16 @@
+"use client";
 import Navbar from "@/components/Navbar";
+import { useState } from "react";
+import PaymentPage, { PaymentFormData } from "./_components/payment-page";
+import PaymentSuccess from "./_components/payment-success";
 
 export default function AddPayment() {
+  const [success, setSuccess] = useState(false);
+  const handleSubmit = async (data: PaymentFormData) => {
+    console.log(data);
+    setSuccess(true);
+  };
+
   return (
     <div className="flex min-h-screen bg-gray-100">
       <div className="flex-1 flex flex-col pl-10 pr-8 pt-8">
@@ -8,6 +18,12 @@ export default function AddPayment() {
           title="Add Payment"
           description="Manage your payments efficiently."
         />
+        {success ? (
+          <PaymentSuccess />
+        ) : (
+          <PaymentPage handleSubmit={handleSubmit} />
+        )}
+
       </div>
     </div>
   );
